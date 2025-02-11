8d7d6ee601f8f6499f361d370e304cfff5e8b79d
Can <can.pouliquen@gmail.com>
2025-02-11 18:29:55 +0100

    add sklearn solver

---
+++ solvers/sklearn.py
@@ -0,0 +1,34 @@
+from benchopt import BaseSolver, safe_import_context
+
+with safe_import_context() as import_ctx:
+    import numpy as np
+
+    from sklearn.covariance import GraphicalLasso
+
+
+class Solver(BaseSolver):
+
+    name = 'Scikit-Learn'
+
+    parameters = {}
+
+    requirements = ["numpy"]
+
+    def set_objective(self, S, alpha):
+        self.S = S
+        self.alpha = alpha
+
+        self.tol = 1e-18
+        self.model = GraphicalLasso(alpha=self.alpha,
+                                    covariance="precomputed",
+                                    tol=self.tol)
+
+    def run(self, n_iter):
+
+        self.model.max_iter = n_iter
+        self.model.fit(self.S)
+
+        self.Theta = self.model.precision_
+
+    def get_result(self):
+        return dict(Theta=self.Theta)
