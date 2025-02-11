a149cb30657037e61f47ea048ee5a7c04db1ba18
Can <can.pouliquen@gmail.com>
2025-02-11 15:23:04 +0100

    wip add simulated dataset

--- datasets/simulated.py
+++ datasets/simulated.py
@@ -1,43 +1,52 @@
 from benchopt import BaseDataset, safe_import_context
 
+from sklearn.utils import check_random_state
+from sklearn.datasets import make_sparse_spd_matrix
 
-# Protect the import with `safe_import_context()`. This allows:
-# - skipping import to speed up autocompletion in CLI.
-# - getting requirements info when all dependencies are not installed.
 with safe_import_context() as import_ctx:
     import numpy as np
 
 
-# All datasets must be named `Dataset` and inherit from `BaseDataset`
 class Dataset(BaseDataset):
 
-    # Name to select the dataset in the CLI and to display the results.
-    name = "Simulated"
+    name = "Simulated i.i.d. Gaussian"
 
-    # List of parameters to generate the datasets. The benchmark will consider
-    # the cross product for each key in the dictionary.
-    # Any parameters 'param' defined here is available as `self.param`.
     parameters = {
         'n_samples, n_features': [
-            (1000, 500),
-            (5000, 200),
+            (100, 10),
+            (100, 20),
+            (100, 50),
         ],
-        'random_state': [27],
+        'alpha': [0.9],
+        'random_state': [0],
     }
 
-    # List of packages needed to run the dataset. See the corresponding
-    # section in objective.py
-    requirements = []
+    requirements = ["sklearn"]
 
     def get_data(self):
         # The return arguments of this function are passed as keyword arguments
         # to `Objective.set_data`. This defines the benchmark's
         # API to pass data. It is customizable for each benchmark.
 
-        # Generate pseudorandom data using `numpy`.
-        rng = np.random.RandomState(self.random_state)
-        X = rng.randn(self.n_samples, self.n_features)
-        y = rng.randn(self.n_samples)
+        rng = check_random_state(self.random_state)
+        Theta_true = make_sparse_spd_matrix(
+            self.n_features,
+            alpha=self.alpha,
+            random_state=rng)
+
+        Theta_true += 0.1*np.eye(self.n_features)
+        Sigma_true = np.linalg.pinv(Theta_true, hermitian=True)
+        X = rng.multivariate_normal(
+            mean=np.zeros(self.n_features),
+            cov=Sigma_true,
+            size=self.n_samples,
+        )
+        S = np.cov(X, bias=True, rowvar=False)
+        S_cpy = np.copy(S)
+        np.fill_diagonal(S_cpy, 0.)
+        lmbd_max = np.max(np.abs(S_cpy))
 
         # The dictionary defines the keyword arguments for `Objective.set_data`
-        return dict(X=X, y=y)
+        return dict(S=S,
+                    Theta_true=Theta_true,
+                    lmbd_max=lmbd_max)
