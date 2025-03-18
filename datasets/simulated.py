511ccae3d160db14a496a6bb4b8238ded5b08196
Can <can.pouliquen@gmail.com>
2025-03-18 11:02:48 +0100

    fix dataset

--- datasets/simulated.py
+++ datasets/simulated.py
@@ -15,10 +15,7 @@ class Dataset(BaseDataset):
         'n_samples, n_features': [
             (1000, 50),
             (1000, 100),
-            (1000, 250),
-            (1000, 400),
-            (1000, 800),
-            (1000, 1000),
+            (1000, 200),
         ],
         'sparsity_controller': [0.9],
         'random_state': [0],
